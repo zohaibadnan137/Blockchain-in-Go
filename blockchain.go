d1b513f1417d2c0620be875f388f098cc3a235c4
zohaibadnan137 <zohaibadnan137@gmail.com>
2022-10-25 12:12:37 +0500

    Add skeleton code for public functions

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package assignment01bca
 
 type Block struct {
 	next         *Block
@@ -8,6 +6,39 @@ type Block struct {
 	prevHash     string
 }
 
-func main() {
-	fmt.Println("hello world")
+// ******** ******** PUBLIC FUNCTIONS ******** ******** //
+
+// Creates a new block
+func NewBlock() {
+
+}
+
+// Finds the nonce value for a block
+func MineBlock() {
+
+}
+
+// Prints all the blocks in the blockchain
+func DisplayBlocks() {
+
+}
+
+// Prints all the transactions in a block
+func DisplayMerkelTree() {
+
+}
+
+// Changes one or multiple transactions in a block
+func ChangeBlock() {
+
+}
+
+// Verifies whether any changes were made in the blockchain
+func VerifyChain() {
+
+}
+
+// Calculates the hash of a transaction or block
+func CalculateHash() {
+
 }
---
+++ main.go
@@ -0,0 +1,7 @@
+package assignment01bca
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
