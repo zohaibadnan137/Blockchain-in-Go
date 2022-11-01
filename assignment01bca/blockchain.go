dff03bd456db73faadb920249efff1c868668d62
zohaibadnan137 <zohaibadnan137@gmail.com>
2022-11-01 18:07:01 +0500

    Finalize required structures

--- assignment01bca/blockchain.go
+++ assignment01bca/blockchain.go
@@ -1,55 +1,92 @@
 package assignment01bca
 
-// Structs to be used
-type Block struct {
-	// Hash of the current block
-	Hash []byte
-	// Data i.e our transaction(s)
-	Data []byte
-	// Hash of the previous block
-	PrevHash []byte
-	// Random number used to solve the crypto puzzle
-	Nounce int
+import (
+	"time"
+)
+
+// ******** ******** STRUCTURES ******** ******** //
+
+type Transaction struct {
+	id   byte
+	data map[string]interface{}
 }
 
-// Represents the Blockchain
-type Blockchain struct {
-	Chain []*Block
+type Node struct {
+	transaction *Transaction
+	hash        byte
+
+	left  *Node
+	right *Node
 }
+
+type MerkleTree struct {
+	root         *Node
+	transactions []Transaction
+}
+
+type Block struct {
+	hash         byte
+	previousHash byte
+
+	merkleTree MerkleTree
+
+	timestamp time.Time
+	nonce     byte
+}
+
+type Blockchain struct {
+	genesisBlock Block
+	chain        []Block
+}
+
+// ******** ******** PRIVATE FUNCTIONS ******** ******** //
 
 // ******** ******** PUBLIC FUNCTIONS ******** ******** //
 
-// Creates a new block
+// Creates a genesis block
+func CreateBlockchain() Blockchain {
+	genesisBlock := Block{
+		timestamp: time.Now(),
+	}
+
+	return Blockchain{
+		genesisBlock,
+		[]Block{genesisBlock},
+	}
+
+}
+
+// Creates a new block*
 func NewBlock() {
 
 }
 
-// Finds the nonce value for a block
+// Finds the nonce value for a block*
 func MineBlock() {
 
 }
 
-// Prints all the blocks in the blockchain
+// Prints all the blocks in the blockchain*
 func DisplayBlocks() {
 
 }
 
-// Prints all the transactions in a block
+// Prints all the transactions in a block*
 func DisplayMerkelTree() {
 
 }
 
-// Changes one or multiple transactions in a block
+// Changes one or multiple transactions in a block*
 func ChangeBlock() {
 
 }
 
-// Verifies whether any changes were made in the blockchain
+// Verifies whether any changes were made in the blockchain*
 func VerifyChain() {
 
 }
 
-// Calculates the hash of a transaction or block
+// Calculates the hash of a transaction or block*
 func CalculateHash() {
 
 }
--- main.go
+++ main.go
@@ -7,4 +7,6 @@ import (
 func main() {
 	fmt.Println("hello world")
 
+	//var Blockchain assignment01bca.Block
+
 }
